3fe02928863402c7e36e8760427c94ca6b419aec
kiru <kiruthegreat2@gmail.com>
2021-10-07 23:22:58 -0500

    start improvement

--- start.py
+++ start.py
@@ -1,22 +1,41 @@
 import json
+import time
 from town import town
 
 
 def start():
-    print("Welcome to the adventuring guild! Please register with the guild")
+    print("Welcome to the adventuring guild! Please register with the guild.")
 
     playerNameInput = input("Enter a name for your character: ")
 
-    # create and overwrite player variables
-    playerData = open("playerVariables.json", "w")
-    playerData.write("{\"player\": [ {\"playerName\": \"" + playerNameInput + "\",\"playerMaxHealth\": 100, "                                                                             "\"strength\": 5, \"stamina\": 5, "                                                                              "\"magic\": 5}]}")
-    playerData = open("playerVariables.json", "r")
-    jsonPlayerData = json.load(playerData)
+    templatePlayerData = '{"playerData": [ { "playerName": "' + playerNameInput + '", "playerMaxHealth": 100, ' \
+                                                                                  '"strength": 5, "stamina": 5, ' \
+                                                                                  '"magic": 5 } ], "playerItems": [ { ' \
+                                                                                  '"itemName": "" }, { "itemName": "" ' \
+                                                                                  '}, { "itemName": "" }, ' \
+                                                                                  '{ "itemName": "" }, { "itemName": ' \
+                                                                                  '"" }, { "itemName": "" }, ' \
+                                                                                  '{ "itemName": "" }, { "itemName": ' \
+                                                                                  '"" }, { "itemName": "" } ] } '
 
-    playerName = jsonPlayerData["player"][0]["playerName"]
-    print("Your name is " + playerName)
+    player = json.loads(templatePlayerData)
 
-    print("Looks like your all set! Dont forget to pick up your adventurer starter set at the shop.\nYou received a "
-          "starter set voucher.")
-    town()
+    player["playerItems"][0]["itemName"] = "Starter Kit Voucher"
+
+    playerFile = open("playerVariables.json", "w")
+    playerFile.write(json.dumps(player))
+    # playerData = open("playerVariables.json", "r")
+    # jsonPlayerData = json.load(playerData)
+
+    time.sleep(1)
+
+    print("\nYour name is " + playerNameInput + "\nYour stats are " + str(
+        player["playerData"][0]["strength"]) + " strength, " + str(
+        player["playerData"][0]["stamina"]) + " stamina, and " + str(player["playerData"][0]["magic"]) + " magic.")
+
+    time.sleep(2)
+
+    print("\nLooks like your all set! Dont forget to pick up your adventurer starter set at the shop.\nYou received a "
+          "starter set voucher!")
+    # town()
 # adventure hall where you register to be a adventure and go on your first quest
