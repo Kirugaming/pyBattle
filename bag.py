e466259e9a13dcc02041186d296741d047023b9b
Kiru <kiruthegreat2@gmail.com>
2021-11-26 17:00:11 -0600

    refactoring - moved bag functions to own file - using f strings :D

---
+++ bag.py
@@ -0,0 +1,61 @@
+def print_bag(player_data):
+    print("Inside of your bag is...")
+    for bag_index, item_in_bag in enumerate(player_data.bag, start=0):
+        if player_data.bag[0] == "":
+            print("Your bag is empty :(")
+            return player_data
+        elif player_data.bag[bag_index]:
+            print(f'[{bag_index + 1} : {player_data.bag[bag_index].name()}]')
+
+
+def access_bag(player_data):
+    print_bag(player_data)
+
+    bag_action = input("Pick a item number to access\n[bag number][leave]")
+    if bag_action.lower() == "leave":
+        return player_data
+    item_action = input(
+        "What would you like to do with " + player_data.bag[int(bag_action) - 1].name() + "\n[Equip][Use]["
+                                                                                          "Trash]")
+    if item_action.lower() == "equip":
+        equip_item(player_data, bag_action)
+
+    if item_action.lower() == "use" and player_data.bag[int(bag_action) - 1].item_id()[0] == "C":
+        use_item(player_data, bag_action)
+
+    if item_action.lower() == "trash":
+        trash_item(player_data, bag_action)
+
+
+def equip_item(player_data, bag_action):
+    if player_data.bag[int(bag_action) - 1] == "":
+        print("There is nothing to equip.")
+    if player_data.bag[int(bag_action) - 1].item_id()[0] == "W":
+        swap_equips(player_data, "weapon", bag_action)
+    elif player_data.bag[int(bag_action) - 1].item_id()[0] == "A":
+        swap_equips(player_data, "armor", bag_action)
+    else:
+        print("You can not equip this")
+
+    access_bag(player_data)
+
+
+def swap_equips(player_data, item_type, bag_action):
+    item_withholding = player_data.equips[item_type]
+    player_data.equips[item_type] = player_data.bag[int(bag_action) - 1]
+    player_data.bag[int(bag_action) - 1] = item_withholding
+    print(f"{player_data.equips[item_type].name()} was equipped\n")
+
+
+def use_item(player_data, bag_action):
+    # Consume item and give whatever depending on item
+    pass
+
+
+def trash_item(player_data, bag_action):
+    trash_question = input("Are you sure you want to trash this item?[y/n]")
+    if trash_question.lower() == "y":
+        print(f"{player_data.bag[int(bag_action) - 1].name()} Has been trashed.")
+        player_data.bag[int(bag_action) - 1] = ""
+    elif trash_question.lower() == "n":
+        access_bag(player_data)
--- player.py
+++ player.py
@@ -1,69 +1,6 @@
 import item
 
 
-def access_bag(player_data):
-    print("Inside of your bag is...")
-    for bag_index, item_in_bag in enumerate(player_data.bag, start=0):
-        if player_data.bag[0] == "":
-            print("Your bag is empty :(")
-            return player_data
-        elif player_data.bag[bag_index]:
-            print("[" + str(bag_index + 1) + " : " + (player_data.bag[bag_index].name()) + "]")
-
-    bag_action = input("Pick a item number to access\n[bag number][leave]")
-    if bag_action.lower() == "leave":
-        return player_data
-    item_action = input(
-        "What would you like to do with " + player_data.bag[int(bag_action) - 1].name() + "\n[Equip][Use]["
-                                                                                          "Trash]")
-    if item_action.lower() == "equip":
-        equip_item(player_data, bag_action)
-
-    if (
-        item_action.lower() == "use"
-        and player_data.bag[int(bag_action) - 1].item_id()[0] == "C"
-    ):
-        # Consume item and give whatever depending on item
-        pass
-    if item_action.lower() == "trash":
-        trash_question = input("Are you sure you want to trash this item?[y/n]")
-        if trash_question.lower() == "y":
-            print(player_data.bag[int(bag_action) - 1].name() + " Has been trashed.")
-            player_data.bag[int(bag_action)-1] = ""
-        elif trash_question.lower() == "n":
-            access_bag(player_data)
-
-
-def equip_item(player_data, bag_action):
-    if player_data.bag[int(bag_action) - 1] == "":
-        print("There is nothing to equip.")
-    if player_data.bag[int(bag_action) - 1].item_id()[0] == "W":
-        swap_equips(player_data, "weapon", bag_action)
-    elif player_data.bag[int(bag_action) - 1].item_id()[0] == "A":
-        swap_equips(player_data, "armor", bag_action)
-    else:
-        print("You can not equip this")
-
-    access_bag(player_data)
-
-
-def swap_equips(player_data, item_type, bag_action):
-    item_withholding = player_data.equips[item_type]
-    player_data.equips[item_type] = player_data.bag[int(bag_action) - 1]
-    player_data.bag[int(bag_action) - 1] = item_withholding
-    print(player_data.equips[item_type].name() + " was equipped\n")
-
-
-def use_item():
-    print("ITEM was used\n")
-    # TODO: show item is used and trigger behavior item uses
-
-
-def trash_item():
-    print("ITEM was tossed\n")
-    # TODO: show item was tossed and remove item from bag
-
-
 class Player:
 
     def __init__(self, name, strength, stamina, magic):
@@ -119,22 +56,6 @@ class Player:
     def gold(self, new_gold):
         self._gold += new_gold
 
-    def access_bag(self):
-        print("Inside of your bag is...\n")
-        # TODO: print bag contents and actions below items
-
-    def equip_item(self):
-        print("ITEM was equipped\n")
-        # TODO: show item is equipped and equip player with item
-
-    def use_item(self):
-        print("ITEM was used\n")
-        # TODO: show item is used and trigger behavior item uses
-
-    def trash_item(self):
-        print("ITEM was tossed\n")
-        # TODO: show item was tossed and remove item from bag
-
 
 def player_creation():
     print("Welcome to the adventuring guild! Please register with the guild.")
@@ -143,9 +64,7 @@ def player_creation():
 
     player_data = Player(name_input, 5, 5, 5)
 
-    print("\nYour name is " + player_data.name + ".\nYour stats are " + str(player_data.stats["strength"]) +
-          " Strength, " +
-          str(player_data.stats["stamina"]) + " Stamina, and " + str(player_data.stats["magic"]) + " Magic.")
+    print(f"\nYour name is {player_data.name}.\nYour stats are {str(player_data.stats['strength'])}  Strength, {str(player_data.stats['stamina'])} Stamina, and {str(player_data.stats['magic'])} Magic.")
 
     # give player a voucher
     player_data.bag[0] = item.get_item_by_id("K00001")
